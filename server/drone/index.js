8000e3a6479c2d69f61020baaf6472cd46e7d40a
andywilliams <andy@futureadlabs.com>
2013-08-18 11:53:43 +0100

    logging

--- server/drone/index.js
+++ server/drone/index.js
@@ -5,10 +5,14 @@ var flying = false;
 exports.initiate = function() {
   client.takeoff();
   flying = true;
+
+  console.log('taking off');
 };
 
 exports.land = function() {
   client.stop();
   client.land();
   flying = false;
+
+  console.log('landing');
 };
